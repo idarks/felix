0abe8c7aae6b2db396a84ff0c711c15976aad639
Max Syrykh <slogger1994@gmail.com>
2013-12-02 00:35:41 +0600

    Add all file

---
+++ js/faces/locale.flip.js
@@ -0,0 +1,28 @@
+(function($) {
+		
+	/**
+	 * FlipClock English Language Pack
+	 *
+	 * This class will used to translate tokens into the English language.
+	 *	
+	 */
+	 
+	FlipClock.Lang.Russian = {
+		
+		'years'   : ' ',
+		'months'  : ' ',
+		'days'    : ' ',
+		'hours'   : ' ',
+		'minutes' : ' ',
+		'seconds' : ' '	
+
+	};
+	
+	/* Create various aliases for convenience */
+
+	FlipClock.Lang['ru']      = FlipClock.Lang.Russian;
+	FlipClock.Lang['ru-ru']   = FlipClock.Lang.Russian;
+	FlipClock.Lang['russian'] = FlipClock.Lang.Russian;
+
+
+}(jQuery));
